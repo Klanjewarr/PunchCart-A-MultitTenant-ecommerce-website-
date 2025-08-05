36cf056febc5033d8743670724f6299681c36de0
Kanchankumar Lanjewar <kanchankumar200312@gmail.com>
2025-08-05 16:06:36 +0530

    03: home layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,13 @@
+
+
+ export const Footer = () => {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+        <div className="flex flex-col min-h-screen"> 
+            <p>PunchCart, Inc.</p>
+        </div>
+    </footer>
+  )
+}
+
+
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,13 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
+import { useEffect } from "react";
+
 
 export default function Home() {
+
+ 
+
  return(
   <div className="p-4">
     <div className="flex flex-col gap-y-4 ">
