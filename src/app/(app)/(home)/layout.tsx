365725a564acb7a856d0ee6844b0255824876985
Kanchankumar Lanjewar <kanchankumar200312@gmail.com>
2025-08-24 22:02:52 +0530

    10: categoey pages

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -2,12 +2,12 @@
 
 import { trpc, getQueryClient } from "@/trpc/server";
 
-import { Footer } from "./footer";
-import { Navbar } from "./navbar";
+import { Footer } from "@/modules/home/ui/components/footer";
+import { Navbar } from "@/modules/home/ui/components/navbar";
 
 
 
-import { SearchFilters, SearchFiltersSkeleton } from "./search-filters";
+import { SearchFilters, SearchFiltersSkeleton } from "@/modules/home/ui/components/search-filters";
 
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { CategoryDropdown } from "./category-dropdown";
-import { CustomCategory } from "../types";
+
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ListFilterIcon } from "lucide-react";
 import { CategoriesSidebar } from "./categories-sidebar";
+import { useParams } from "next/navigation";
 
 interface CategoriesProps {
-    data: CustomCategory[];
+    data: CategoriesGetManyOutput;
 };
 
 export const Categories = ({ data }: CategoriesProps) => {
+    const params = useParams();
+
     const containerRef = useRef<HTMLDivElement>(null);
     const measureRef = useRef<HTMLDivElement>(null);
     const viewAllRef = useRef<HTMLDivElement>(null);
@@ -20,7 +25,8 @@ export const Categories = ({ data }: CategoriesProps) => {
     const [isAnyHovered, setIsAnyHovered] = useState(false);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const activeCategory = "all";
+    const categoryParam = params.category as string | undefined;
+    const activeCategory = categoryParam || "all";
     const activeCategoryIndex = data.findIndex((cat) => cat.slug === activeCategory);
     const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex != -1;
 
@@ -59,7 +65,7 @@ export const Categories = ({ data }: CategoriesProps) => {
     return (
 
         <div className="relative w-full" >
-            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen}   />
+            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
 
             <div
                 ref={measureRef}
@@ -79,12 +85,12 @@ export const Categories = ({ data }: CategoriesProps) => {
             </div>
 
             {/*Visible categories container*/}
-            <div 
-            ref={containerRef}
-            onMouseEnter={() => setIsAnyHovered(true)}
-            onMouseLeave={() => setIsAnyHovered(false)}
-            className="flex flex-nowrap items-center">
-                {data.slice(0,visibleCount).map((category) => (
+            <div
+                ref={containerRef}
+                onMouseEnter={() => setIsAnyHovered(true)}
+                onMouseLeave={() => setIsAnyHovered(false)}
+                className="flex flex-nowrap items-center">
+                {data.slice(0, visibleCount).map((category) => (
                     <div key={category.id}>
                         <CategoryDropdown
                             category={category}
@@ -95,20 +101,21 @@ export const Categories = ({ data }: CategoriesProps) => {
 
                 ))}
 
-            <div
-            ref={viewAllRef}
-            className="shrink-0 "
-            >
-                <Button
-                className={cn(
-                    "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-                    isActiveCategoryHidden && !isAnyHovered && "bg-white border-primary",)}
-                onClick={() => setIsSidebarOpen(true)}
+                <div
+                    ref={viewAllRef}
+                    className="shrink-0 "
                 >
-                    View All
-                    <ListFilterIcon className="ml-2 " />
-                </Button>
-            </div>
+                    <Button
+                        variant="elevated"
+                        className={cn(
+                            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+                            isActiveCategoryHidden && !isAnyHovered && "bg-white border-primary",)}
+                        onClick={() => setIsSidebarOpen(true)}
+                    >
+                        View All
+                        <ListFilterIcon className="ml-2 " />
+                    </Button>
+                </div>
 
             </div>
         </div>
