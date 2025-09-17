b639a159ad50d58bf3ef4d656164d0c8c5d9cf47
Kanchankumar Lanjewar <kanchankumar200312@gmail.com>
2025-09-17 23:29:29 +0530

    22: Reviews

---
+++ src/modules/library/ui/components/review-sidebar.tsx
@@ -0,0 +1,24 @@
+import { useTRPC } from "@/trpc/client";
+import {  useSuspenseQuery } from "@tanstack/react-query";
+import { ReviewForm } from "./review-form";
+
+interface Props{
+    productId:string;
+}
+
+export const ReviewSidebar = ({productId}: Props) =>{
+    const trpc = useTRPC();
+    // when use useSuspense query you need to have a matching prefecthQuery(for useQury you don't)
+    const { data } = useSuspenseQuery(trpc.reviews.getOne.queryOptions({
+        productId,
+    }))
+    
+    return(
+        <div>
+           <ReviewForm 
+           productId={productId}
+           initialData={data}
+           />
+        </div>
+    )
+}
